400424d21946ff93a16854828f0bc95dbfd4bf34
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-09-25 11:49:55 +0100

    bootrr: upgrade with support for ACPI and new devices
    Upgrade bootrr to kernelci-20200925 which adds support for determining
    the platform name on ACPI devices, and test scripts for the "hana" and
    "grunt" Chromebooks.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- package/bootrr/bootrr.mk
+++ package/bootrr/bootrr.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 BOOTRR_SITE = https://github.com/kernelci/bootrr.git
-BOOTRR_VERSION = e0a316de59153ecd4373b14063b0280505ce6e2e
+BOOTRR_VERSION = fdd52e16572ec16811a1303f4d781d9ba4195f07 # kernelci-20200925
 BOOTRR_SITE_METHOD = git
 BOOTRR_LICENSE = LGPL-2.1+
 
